7e4aa636bac0c282edf559d6b9fc2433b75e2bb5
tom_glb <goodMoring_glb@qq.com>
2019-07-19 12:43:36 +0800

    test conflict

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class Happy {
 	public static void main(String[] args) {
-		System.out.println("Testing ....");
+		System.out.println("Testing testgit1 ....");
 	}
 	
 
