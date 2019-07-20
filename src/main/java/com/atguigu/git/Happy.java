5d156e9c027133740aeaf96c3e3ac858c7722c1d
Eclipse_user <Eclipse_user@qq.com>
2019-07-20 09:39:42 +0800

    test branch hot_fix

--- src/main/java/com/atguigu/git/Happy.java
+++ src/main/java/com/atguigu/git/Happy.java
@@ -3,6 +3,7 @@ package com.atguigu.git;
 public class Happy {
 	public static void main(String[] args) {
 		System.out.println("Testing testgit2....");
+		System.out.println("Testing branch hot_fix");
 	}
 	
 
